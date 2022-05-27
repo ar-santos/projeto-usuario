fc492c9481a6dc014ff57d594efe11277b6f2bd5
ArthurSantos54 <Arthursantos4367@gmail.com>
2022-05-27 19:22:59 -0300

    Começo javaScript

---
+++ src/style/js/usuario.js
@@ -0,0 +1,9 @@
+let mostrarEoculta = document.getElementById("less")
+
+function mostrar() {
+    if (mostrarEoculta.style.display == 'block'){
+        mostrarEoculta.style.display = 'none';
+}else { 
+    mostrarEoculta.style.display = 'block' 
+}
+}
