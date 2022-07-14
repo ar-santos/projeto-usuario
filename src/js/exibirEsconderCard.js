d49273c06dfdfaebc861f530b7e2b9f35c45a029
ArthurSantos54 <Arthursantos4367@gmail.com>
2022-07-13 21:35:10 -0300

    Responsivo-css

--- src/js/exibirEsconderCard.js
+++ src/js/exibirEsconderCard.js
@@ -2,13 +2,28 @@ let card = document.getElementById("id-card")
 let closeIcon = document.getElementById("ocultarCard") 
 
 function ocultarCard() {
-   if (card.style.display == 'block') {
+   if (closeIcon.style.display == 'none') {
       card.style.display = 'none';
       closeIcon.style.display = 'none';
    } else { 
    card.style.display = 'block' 
    closeIcon.style.display = 'block' 
-   }
+   } 
 }
 
 export default ocultarCard 
+
+// let card = document.getElementById("id-card") 
+// let closeIcon = document.getElementById("ocultarCard") 
+
+// function ocultarCard() {
+//    if (card.style.display == 'block') {
+//       card.style.display = 'none';
+//       closeIcon.style.display = 'none';
+//    } else { 
+//    card.style.display = 'block' 
+//    closeIcon.style.display = 'block' 
+//    } 
+// }
+
+// export default ocultarCard 
