e65813828f26c873dc5b99cc0f2e9a8739a8efc5
ArthurSantos54 <Arthursantos4367@gmail.com>
2022-07-30 18:03:53 -0300

    revisão-4

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,11 @@ let button = document.getElementById("primeiro-button") // Trocar nome do icone 
 let textoDoInput = document.getElementById("id-pesquisa")
 import pegarUsuario from './usuario.js'
 import utils from './exibirEsconderCard.js'
+import Debug from './pegarDados.js'
 let card = document.getElementById("id-card") 
 let closeIcon = document.getElementById("ocultarCard") 
+
+Debug()
 
 function pesquisarUsuario() { 
     pegarUsuario()
---
+++ src/js/pegarDados.js
@@ -0,0 +1,11 @@
+let addressCard = document.getElementById("address-id")
+let sobreCard = document.getElementById("sobre-id")
+let companyCard = document.getElementById("company-id")
+
+function debug() {
+console.log(addressCard)
+console.log(sobreCard)
+console.log(companyCard)
+}
+
+export default debug
