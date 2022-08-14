011203bc4155cbe55437e09602c1734b37333763
ArthurSantos54 <Arthursantos4367@gmail.com>
2022-08-14 13:46:56 -0300

    Pesquisa exibindo no card.

--- src/js/pegarDados.js
+++ src/js/pegarDados.js
@@ -9,6 +9,8 @@ function pegarDados() {
     for (let usuario of dados) {
         if(usuario.id == resultadoPesquisado){
             sobreDataCard(usuario, sobreCard)        
+            companyDataCard(usuario, companyCard)        
+            addressDataCard(usuario, addressCard)        
         }
     }   
 }
@@ -18,20 +20,71 @@ function criarElemento() {
    return newElement
 }
 
+
 function sobreDataCard(data, card) {
+    const id = criarElemento()
     const nome = criarElemento()
     const nomeUsuario = criarElemento()
     const email = criarElemento()
+    const phone = criarElemento()
+    const website = criarElemento()
 
+    id.textContent = `Id: ${data.id}`
     nome.textContent = `Name: ${data.name}`
     nomeUsuario.textContent = `User: ${data.username}`
     email.textContent = `Email: ${data.email}`
+    phone.textContent = `Phone: ${data.phone}`
+    website.textContent = `Website: ${data.website}`
 
-
+    card.appendChild(id)
     card.appendChild(nome)
     card.appendChild(nomeUsuario)
     card.appendChild(email)
+    card.appendChild(phone)
+    card.appendChild(website)
 } 
+
+
+function companyDataCard(data, card) {
+    const name = criarElemento()
+    const catchPhrase = criarElemento()
+    const bs = criarElemento()
+
+    name.textContent = `Name: ${data.company.name}`
+    catchPhrase.textContent = `CatchPhrase: ${data.company.catchPhrase}`
+    bs.textContent = `Bs: ${data.company.bs}`
+
+    card.appendChild(name)
+    card.appendChild(catchPhrase)
+    card.appendChild(bs)
+}
+
+
+function addressDataCard(data, card) {
+    const street = criarElemento()
+    const suite = criarElemento()
+    const city = criarElemento()
+    const zipcode = criarElemento()
+    const geo = criarElemento()
+    const lat = criarElemento()
+    const lng = criarElemento()
+
+    street.textContent = `Street: ${data.address.street}`
+    suite.textContent = `Suite: ${data.address.suite}`
+    city.textContent = `City: ${data.address.city}`
+    zipcode.textContent = `Zipcode: ${data.address.zipcode}`
+    // geo.textContent = `Geo {} ${data.address.geo}`
+    lat.textContent = `Lat: ${data.address.geo.lat}`
+    lng.textContent = `Ing: ${data.address.geo.lng}`
+
+    card.appendChild(street)
+    card.appendChild(suite)
+    card.appendChild(city)
+    card.appendChild(zipcode)
+    // card.appendChild(geo)
+    card.appendChild(lat)
+    card.appendChild(lng)
+}
 
 
 {/* 
